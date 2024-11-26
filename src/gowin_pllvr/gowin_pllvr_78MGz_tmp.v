64127fabc84ae0423dd0b33ba85d001ca1476ac6
aesafov <aesafov@yandex.ru>
2024-11-26 22:44:51 +0300

    Добавил проект моргалки с проца на светодиод TANG NANO 4K

---
+++ src/gowin_pllvr/gowin_pllvr_78MGz_tmp.v
@@ -0,0 +1,18 @@
+//Copyright (C)2014-2024 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.9.03 Education (64-bit)
+//Part Number: GW1NSR-LV4CQN48PC6/I5
+//Device: GW1NSR-4C
+//Created Time: Mon Nov 25 02:37:55 2024
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_PLLVR_78MGz your_instance_name(
+        .clkout(clkout), //output clkout
+        .lock(lock), //output lock
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
